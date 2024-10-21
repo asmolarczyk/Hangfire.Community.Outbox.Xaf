28321d0bdccfede9d58afb308aeb7a514e45425a
BredStik <mathieulafontaine@hotmail.com>
2024-10-20 23:44:52 -0400

    Renamed project to Hangfire.Outbox

--- src/Hangfire.EfCore.Outbox/Extensions/TypeExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Hangfire.EfCore.Outbox.Extensions;
-
-public static class TypeExtensions
-{
-    public static string GetFriendlyName(this Type type)
-    {
-        return $"{type.FullName}, {type.Assembly.GetName().Name}";
-    }
-}
